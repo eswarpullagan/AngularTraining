c77aaa20de57f8e7a0670ed5394939fea3f53d87
Eswar <eswar.pullagan@gmail.com>
2019-12-28 23:30:36 +0530

    PropertyBinding-Lab3

--- src/app/property-binding/property-binding.component.ts
+++ src/app/property-binding/property-binding.component.ts
@@ -7,12 +7,22 @@ import { Component, OnInit } from '@angular/core';
               {{name}}
               </h2>
               <input [id]="myId" type="text" value="Eswar">
+              <input id="{{myId}}" type="text" value="Eswar">
+              <input disabled id="{{myId}}" type="text" value="Eswar">
+              <input disabled=false id="{{myId}}" type="text" value="Eswar">
+              <input disabled="false" id="{{myId}}" type="text" value="Eswar">
+              <input disabled="{{false}}" id="{{myId}}" type="text" value="Eswar">
+              <input [disabled]="false" id="{{myId}}" type="text" value="Eswar">
+              <input [disabled]="true" id="{{myId}}" type="text" value="Eswar">
+              <input [disabled]="isDisabled" id="{{myId}}" type="text" value="Eswar">
+              <input bind-disabled="isDisabled" id="{{myId}}" type="text" value="Eswar">
             <div>`,
   styleUrls: ['./property-binding.component.css']
 })
 export class PropertyBindingComponent implements OnInit {
   public name="Angular Training"
   public myId="testId"
+  public isDisabled=false;
   constructor() { }
 
   ngOnInit() {
