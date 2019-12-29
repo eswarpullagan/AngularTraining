c1e3ca205971f5f8faf858072a302b654db902b6
Eswar <eswar.pullagan@gmail.com>
2019-12-29 09:16:35 +0530

    ngClass Directive

--- src/app/class-binding/class-binding.component.ts
+++ src/app/class-binding/class-binding.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   <h2 class="text-success">Angular Training</h2> 
   <h2 [class]="successClass">Angular Training - Class Property</h2>   
   <h2 class="text-special" [class]="successClass">Angular Training - Attribute and Class Property- Attribute becomes dummy</h2> 
-  <h2 [class.text-danger]="hasError">Angular Training - Class Property- Conditional Class</h2>   
+  <h2 [class.text-danger]="hasError">Angular Training - Class Property- Conditional Class</h2> 
+  <h2 [class]="messageClasses">Angular Training- ngClass Directive</h2>
+  <h2 [ngClass]="messageClasses">Angular Training- ngClass Directive- Allows you to dynamically add or remove classes based on conditions</h2>   
   `,
   styles:[`
     .text-success{
@@ -25,6 +27,12 @@ export class ClassBindingComponent implements OnInit {
   name="Eswar"
   successClass="text-success"
   hasError=true
+  isSpecial=true
+  messageClasses={
+    "text-success":!this.hasError,
+    "text-danger":this.hasError,
+    "text-special":this.isSpecial
+  }
   constructor() { }
 
   ngOnInit() {
