211ed46c480b0adb0a2baf4e5a0842e06a2fcf05
Eswar <eswar.pullagan@gmail.com>
2019-12-30 07:54:41 +0530

    Information About Event

--- src/app/event-binding/event-binding.component.ts
+++ src/app/event-binding/event-binding.component.ts
@@ -4,7 +4,8 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-event-binding',
   template: ` 
   <h2>Angular Training</h2>
-  <button (click)="onClick()">Click Here</button>
+  <button (click)="onClick($event)">Click Here</button>
+  <button (click)="name='Eswar'">Click Here</button>
   <h2>Welcome {{name}}</h2>
   `,
   styleUrls: ['./event-binding.component.css']
@@ -15,10 +16,11 @@ export class EventBindingComponent implements OnInit {
 
   ngOnInit() {
   }
-  onClick()
+  onClick(event)
   {
     console.log("Welcome to Angular Training")
-    this.name="Eswar"
+    console.log(event)
+    this.name=event.type
   }
 
 }
